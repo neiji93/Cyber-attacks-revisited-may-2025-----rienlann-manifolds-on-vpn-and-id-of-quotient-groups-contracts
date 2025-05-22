eb6595e6dc81d2cc2d5342d2f5c773f2c2438a2b
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-05-22 11:22:19 +0200

    Create NonConstantTermsCrawlerHeaders.h

---
+++ NonConstantTermsCrawlerHeaders.h
@@ -0,0 +1,6 @@
+//we can find usefull datas on networks
+//if we prevent quantizers. on the other hand,
+//in our gpu chips jamming, we believed that
+//there is nobdirect access to the full power
+//of quantizers.
+
